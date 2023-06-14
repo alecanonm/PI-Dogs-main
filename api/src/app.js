d87790b1839075601f28645c103ee2926f1332eb
Alejandro <you@example.com>
2023-06-14 18:52:31 -0500

    a little error has been fixed

--- api/src/app.js
+++ api/src/app.js
@@ -19,7 +19,7 @@ server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://pi-dogs-main-cyan.vercel.app/"
+    "https://pi-dogs-main-cyan.vercel.app"
   ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
