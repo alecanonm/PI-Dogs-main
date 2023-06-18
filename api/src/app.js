8df4b65d4edc15d597d4c1ce772c9e24c2e6b018
Alejandro <you@example.com>
2023-06-18 18:01:32 -0500

    i fixed an error into server side

--- api/src/app.js
+++ api/src/app.js
@@ -19,7 +19,7 @@ server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://pi-dogs-main-l8690w1fa-alecanonm.vercel.app"
+    "https://pi-dogs-main-rho.vercel.app"
   ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
